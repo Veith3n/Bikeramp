5e664db7bdbff6cdf276bf0ada41793885387c48
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-08-28 17:29:17 +0200

    update trips_controller
    -allow call to this controller without authenticity token
    - add two params in monthly method (orderParam and orderType)
    - returns JSONs with different codes in different cases

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,10 +1,18 @@
 class TripsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def new
     @trip = Trip.new
   end
 
   def create
-    @trip = Trip.create(trip_params)
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      render json: @trip, status: :created
+    else
+      render json: @trip.errors, status: :unprocessable_entity
+    end
   end
 
   def weekly
@@ -12,10 +20,32 @@ class TripsController < ApplicationController
   end
 
   def monthly
-    render json: TripStats.monthly_stats.to_json
+    if order_param_correct? && order_type_correct?
+      orderParam = sort_params[:orderParam]
+      orderType = sort_params[:orderType]
+      render json: TripPresenter.monthly_stats_serialized(orderParam, orderType).to_json
+    else
+      render json: 'Invalid params', status: :unprocessable_entity
+    end
   end
+
+  private
 
   def trip_params
     params.require(:trip).permit(:start_address, :destination_address, :price)
+  end
+
+  def sort_params
+    params.permit(:orderParam, :orderType)
+  end
+
+  def order_param_correct?
+    correct_values = %w[day total_distance avg_ride avg_price]
+    correct_values.include?(sort_params[:orderParam]) || !sort_params[:orderParam]
+  end
+
+  def order_type_correct?
+    correct_values = %w[asc desc]
+    correct_values.include?(sort_params[:orderType]) || !sort_params[:orderType]
   end
 end
