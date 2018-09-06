84a1abb1ef35ba9647f37a0ecaa7433e157d8d35
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-09-06 11:33:39 +0200

    update tests

--- spec/presenters/trip_presenter_spec.rb
+++ spec/presenters/trip_presenter_spec.rb
@@ -1,29 +1,11 @@
 require 'rails_helper'
+require 'support/trip_repository_mock'
 
 RSpec.describe TripPresenter do
-  context '#monthly_stats_serialized', vcr: 'address_cords' do
-    before do
-      Trip.skip_callback(:validation, :after, :set_date, raise: false)
-    end
-
-    let!(:last_year_trip) do
-      create(:trip, date: 1.year.ago)
-    end
-
-    let!(:first_day_of_current_month_10) do
-      create(:trip, date: Date.current.beginning_of_month)
-    end
-
-    let!(:first_day_of_current_month_20) do
-      create(:trip, date: Date.current.beginning_of_month, price: 20)
-    end
-
-    let!(:last_day_of_current_moth) do
-      create(:trip, date: Date.current.end_of_month)
-    end
-
+  context '#monthly_stats_serialized' do
     it 'return correct data' do
-      monthly_stats = TripPresenter.monthly_stats_serialized(nil, nil)
+      monthly_hash = TripRepositoryMock.monthly_stats
+      monthly_stats = TripPresenter.monthly_stats_serialized(monthly_hash)
 
       expect(monthly_stats.size).to eq(2)
 
@@ -36,32 +18,13 @@ RSpec.describe TripPresenter do
     end
   end
 
-  context '#weekly_stats_serialized', vcr: 'address_cords' do
-    before do
-      Trip.skip_callback(:validation, :after, :set_date, raise: false)
-    end
+  context '#weekly_stats_serialized' do
+    it 'return correctly formatted data' do
+      weekly_hash = TripRepositoryMock.weekly_stats
+      weekly_stats = TripPresenter.weekly_stats_serialized(weekly_hash)
 
-    let!(:last_year_trip) do
-      create(:trip, date: 1.year.ago)
-    end
-
-    let!(:first_day_of_current_week_10) do
-      create(:trip, date: Date.current.beginning_of_week)
-    end
-
-    let!(:first_day_of_current_week_20) do
-      create(:trip, date: Date.current.beginning_of_week, price: 20)
-    end
-
-    let!(:last_day_of_current_week) do
-      create(:trip, date: Date.current.end_of_week)
-    end
-
-    it 'return correct data' do
-      weekly_stats = TripPresenter.weekly_stats_serialized
-
-      expect(weekly_stats[:total_distance]).to eq('5.43km')
-      expect(weekly_stats[:total_price]).to eq('40PLN')
+      expect(weekly_stats[:total_distance]).to eq('50km')
+      expect(weekly_stats[:total_price]).to eq('100PLN')
     end
   end
 end
--- spec/services/trip_stats_spec.rb
+++ spec/services/trip_stats_spec.rb
@@ -1,70 +1,31 @@
 require 'rails_helper'
+require 'support/trip_repository_mock'
 
 RSpec.describe TripStats do
-  context '#monthly_stats', vcr: 'address_cords' do
-    before do
-      Trip.skip_callback(:validation, :after, :set_date, raise: false)
-    end
-
-    let!(:last_year_trip) do
-      create(:trip, date: 1.year.ago)
-    end
-
-    let!(:first_day_of_current_month_10) do
-      create(:trip, date: Date.current.beginning_of_month)
-    end
-
-    let!(:first_day_of_current_month_20) do
-      create(:trip, date: Date.current.beginning_of_month, price: 20)
-    end
-
-    let!(:ereyesterday_day_of_current_moth) do
-      create(:trip, date: Date.current.end_of_month.yesterday)
-    end
-
-    let!(:last_day_of_current_moth) do
-      create(:trip, date: Date.current.end_of_month, destination_address: 'Nowogordzka 50, Warszawa')
-    end
-
+  context '#monthly_stats' do
     it 'return correctly sorted data' do
-      monthly_stats = TripStats.monthly_stats('total_distance', 'desc')
+      repository = TripRepositoryMock
+
+      monthly_stats = TripStats.new(repository).monthly_stats('total_distance', 'desc')
 
       expect(monthly_stats.size).to eq(3)
 
       expect(monthly_stats.first[:day]).to eq(Date.current.beginning_of_month)
-      expect(monthly_stats.first[:total_distance].to_f).to eq(3.62)
-      expect(monthly_stats.first[:avg_ride].to_f).to eq(1.81)
+      expect(monthly_stats.first[:total_distance].to_f).to eq(70)
+      expect(monthly_stats.first[:avg_ride].to_f).to eq(35)
       expect(monthly_stats.first[:avg_price].to_f).to eq(15)
 
       expect(monthly_stats.last[:day]).to eq(Date.current.end_of_month.yesterday)
     end
   end
 
-  context '#weekly_stats', vcr: 'address_cords' do
-    before do
-      Trip.skip_callback(:validation, :after, :set_date, raise: false)
-    end
-
-    let!(:last_year_trip) do
-      create(:trip, date: 1.year.ago)
-    end
-
-    let!(:first_day_of_current_week_10) do
-      create(:trip, date: Date.current.beginning_of_week)
-    end
-
-    let!(:first_day_of_current_week_20) do
-      create(:trip, date: Date.current.beginning_of_week, price: 20)
-    end
-
-    let!(:last_day_of_current_week) do
-      create(:trip, date: Date.current.end_of_week)
-    end
-
+  context '#weekly_stats' do
     it 'return correct data' do
-      weekly_stats = TripStats.weekly_stats
+      repository = TripRepositoryMock
 
-      expect(weekly_stats[:total_distance].to_f).to eq(5.43)
+      weekly_stats = TripStats.new(repository).weekly_stats
+
+      expect(weekly_stats[:total_distance].to_f).to eq(30)
       expect(weekly_stats[:total_price].to_f).to eq(40)
     end
   end
