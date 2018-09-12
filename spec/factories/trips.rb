2a73e783273cf7593415b76b9b021fc6a77c41f0
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-09-12 11:00:52 +0200

    refactor trips factory

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :trip do
-    start_address 'Miodowa 1, Warszawa'
-    destination_address 'Nowogordzka 1, Warszawa'
-    date Date.current
-    price 10
+    start_address { 'Miodowa 1, Warszawa' }
+    destination_address { 'Nowogordzka 1, Warszawa' }
+    date { Date.current }
+    price { 10 }
   end
 end
